443f462977ace8e1af1b514fda0b44ab0c2dd5a8
Julien Prunier <jprunier.1@gmail.com>
2018-09-13 14:34:10 -0400

    new comment

--- performance-metagene2/metagene_chipseq_12.R
+++ performance-metagene2/metagene_chipseq_12.R
@@ -18,7 +18,7 @@ output1 = paste0(args[1],"_", args[2], "cores.RData")
 output2 = paste0(args[1],"_",args[2],"cores.tiff")
 
 listbam = as.character(design$File[design$Experiment > 0])
-
+#new comment
 
 mg <- metagene$new(regions = reg, bam_files = listbam, assay = 'chipseq', cores = as.integer(args[2]),
                    force_seqlevels = TRUE, design = design)
